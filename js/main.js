beb8e007343981843ce6d77d2263bb64785202cb
Liang-Hsuan Ma <leonma333@hotmail.com>
2016-06-25 18:51:12 -0400

    update anchor

--- js/main.js
+++ js/main.js
@@ -34,13 +34,16 @@ function update() {
     currentPlayer.player.body.velocity.y = 0;
 
     currentPlayer.player.rotation = Context.game.physics.arcade.angleToPointer(currentPlayer.player);
+    currentPlayer.player.anchor.set(0.5);
     if (cursors.left.isDown){
         //  Move to the left
         currentPlayer.player.body.velocity.x = -150;
     } else if (cursors.right.isDown){
         //  Move to the right
        currentPlayer.player.body.velocity.x = 150;
-    } else if (cursors.up.isDown) {
+    } 
+
+    if (cursors.up.isDown) {
         // Move up
         currentPlayer.player.body.velocity.y = -150;
     } else if (cursors.down.isDown) {
