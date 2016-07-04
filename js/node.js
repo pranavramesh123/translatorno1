b9f47fdc3d03f9ea3c8ec585b5032b4cf2eee188
Johnson Han <hancanada@hotmail.com>
2016-07-03 20:14:29 -0400

    Node JS Backend Deployment Failed Hot Fix

--- js/node.js
+++ js/node.js
@@ -6,7 +6,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname+'/index.html'));
+	res.sendFile(path.join(__dirname+'/../index.html'));
 });
 
 app.get('/room', function (req, res) {
